31b3a09654c47799329e3abdd75f082b2e184820
Liox <Leonardo.mendieta.programer@gmail.com>
2022-12-09 22:34:08 -0400

    Se completo y se desarrollo el proyecto js-challenge-2

--- js-challenge-2/src/app.js
+++ js-challenge-2/src/app.js
@@ -1,53 +1,50 @@
 (() => {
-  // Exersice 1
-  const ex01Array = [12, 6, 10, 2, 45, 100]
+  // ****************************** EXERCISE 1 ******************************
+ const ex01Array = [12, 6, 10, 2, 45, 100]
 
-  let smallestNumber = ex01Array[0]
-  ex01Array.forEach((item) => {
-    if (item < smallestNumber) {
-      smallestNumber = item
-    }
-  })
-  console.log(smallestNumber)
+ let smallestNumber = ex01Array[0]
+ ex01Array.forEach((item) => {
+   if (item < smallestNumber) {
+     smallestNumber = item
+   }
+ })
+ console.log('****************************** EXERCISE 1 ******************************')
+ console.log(smallestNumber)
 
-  // Exersice 2
+  // ****************************** EXERCISE 2 ******************************
+
   const ex02Array = [3, 'c', 'c', 'a', 2, 3, 'c', 3, 'c', 2, 4, 9, 9]
-  const countElements = {}
-  ex02Array.forEach((item) => {
-    countElements[item] = (countElements[item] || 0) + 1
-  })
-  const countElementsArray = Object.entries(countElements)
-  let lessRepeatedNumber = countElementsArray[0]
-  countElementsArray.forEach((item) => {
-    if (item[1] < lessRepeatedNumber[1]) {
-      lessRepeatedNumber = item
-    }
-  })
-  console.log(lessRepeatedNumber[0])
+  const ex02Array_CountedElements = ex02Array.reduce((countedElements, item) => {
+    countedElements[item] = (countedElements[item] || 0) + 1
+    return countedElements
+  }, {})
+  const minCount = Math.min(...Object.values(ex02Array_CountedElements))
+  const lessFrequent = Object.keys(ex02Array_CountedElements).filter((key) => ex02Array_CountedElements[key] === minCount)
 
-  // Exersice 3
-  const ex03Array = [7, 9, 1, 'a', 'a', 'f', 9, 4, 2, 'd', 'd']
-  const elementsObject = {}
-  ex03Array.forEach((item) => {
-    elementsObject[item] = (elementsObject[item] || 0) + 1
-  })
-  const elementsArray = Object.entries(elementsObject)
-  const uniqueElements = elementsArray.filter((item) => {
-    return item[1] === 1
-  })
+  console.log('****************************** EXERCISE 2 ******************************')
+  console.log(lessFrequent)
 
-  console.log(uniqueElements)
+  // ****************************** EXERCISE 3 ******************************
+   const ex03Array = [7, 9, 1, 'a', 'a', 'f', 9, 4, 2, 'd', 'd']
+   const ex03Array_CountedElements = ex03Array.reduce((countedElements, item) => {
+    countedElements[item] = (countedElements[item] || 0) + 1
+    return countedElements
+   }, {})
+   const uniqueElements = Object.keys(ex03Array_CountedElements).filter((key) => ex03Array_CountedElements[key] === 1)
+  
+   console.log('****************************** EXERCISE 3 ******************************')
+   console.log(uniqueElements)
 
-  // Exersice 4
-  const ex04Array = [
-    ['The', 'little', 'horse'],
-    ['Plane', 'over', 'the', 'ocean'],
-    ['Chocolate', 'ice', 'cream', 'is', 'awesome'],
-    ['this', 'is', 'a', 'long', 'sentence']
-  ]
-  const arrayOfStrings = ex04Array.map((row) => {
-    console.log(row.toString().split(',').join(' '))
-    return row.toString().split(',').join(' ')
-  })
-  console.log(arrayOfStrings)
+   // ****************************** EXERCISE 4 ******************************
+   const ex04Array = [
+     ['The', 'little', 'horse'],
+     ['Plane', 'over', 'the', 'ocean'],
+     ['Chocolate', 'ice', 'cream', 'is', 'awesome'],
+     ['this', 'is', 'a', 'long', 'sentence']
+   ]
+   const arrayOfStrings = ex04Array.map((row) => {
+     return row.toString().split(',').join(' ')
+   })
+   console.log('****************************** EXERCISE 4 ******************************')
+   console.log(arrayOfStrings)
 })()
