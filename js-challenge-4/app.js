cc4b32530f22d547403454063bdb0267c0148ccc
Angel Leonardo Mendieta Castillo <leonardo.mendieta.programer@gmail.com>
2022-12-15 11:02:50 -0400

    js-challenge-4 imprime el tiempo de espera en que se cumple la promesa

--- js-challenge-4/app.js
+++ js-challenge-4/app.js
@@ -1,8 +1,8 @@
 (() => {
   const delay = (ms) => {
     return new Promise((resolve, reject) => {
-      setTimeout(() => { resolve('Done') }, ms)
+      setTimeout(() => { resolve(`Done after ${ms / 1000} seconds`) }, ms)
     })
   }
-  delay(3000).then(() => console.log('Done'))
+  delay(3000).then((res) => console.log(res))
 })()
